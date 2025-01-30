59c6bb46b4a63627d3ca25514301170f77c5ae14
GreyA <grey.altaer@gmail.com>
2025-01-30 09:37:21 -0800

    querySelectors defined for numbers and operations' git push origin main

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ let operator = '';
 
 /*------------------------ Cached Element References ------------------------*/
 
+let numButtons = document.querySelectorAll('.number');
+let opButtons = document.querySelectorAll('.operator');
+let multButton = document.querySelector('.multiply');
+let subButton = document.querySelector('.subtract');
+let clearButton = document.querySelector('.operator.C');
+let eqButton = document.querySelector('.equals');
+let addButton = document.querySelector('.add');
+
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
